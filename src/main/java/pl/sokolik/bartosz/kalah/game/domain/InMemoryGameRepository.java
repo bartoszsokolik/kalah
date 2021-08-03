16c46744989fcfd318b8100d6d3d7d2db9690fb0
bsokolik <bartosz.sokolik@panek.eu>
2021-08-03 23:35:10 +0200

    Use ConcurrentHashMap

--- src/main/java/pl/sokolik/bartosz/kalah/game/domain/InMemoryGameRepository.java
+++ src/main/java/pl/sokolik/bartosz/kalah/game/domain/InMemoryGameRepository.java
@@ -2,14 +2,14 @@ package pl.sokolik.bartosz.kalah.game.domain;
 
 import pl.sokolik.bartosz.kalah.game.domain.exception.GameNotFoundException;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 class InMemoryGameRepository {
 
-  private static final Map<UUID, Game> GAME_MAP = new HashMap<>();
+  private static final Map<UUID, Game> GAME_MAP = new ConcurrentHashMap<>();
 
   Game save(Game game) {
     GAME_MAP.put(game.getId(), game);
